eeaa76be2afcc90c3079dc0acdc725ba4ded226f
tornike512 <dht5152@gmail.com>
2026-02-06 16:03:05 +0400

    fix: not continuing from the last reading manga bug

--- src/features/reader/hooks/use-track-reading.ts
+++ src/features/reader/hooks/use-track-reading.ts
@@ -1,9 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { ReadingHistoryCreate } from "@/types/history.types";
 import { trackReading } from "../api/track-reading";
 
 export const useTrackReading = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (data: ReadingHistoryCreate) => trackReading(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["manga", "slug"] });
+    },
   });
 };
